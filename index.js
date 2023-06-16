f3912c09d165556af79840a5bca69e12cedd6329
AlexGadzhibekov <123484304+AlexGadzhibekov@users.noreply.github.com>
2023-06-16 21:52:39 +0600

    Реализация базовой логики

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import path from "path";
 import mongoose from "mongoose";
+import "dotenv/config";
+
+import viewsRouter from "./routes/views.js";
+import userRouter from "./routes/user.js";
 
 const PORT = 8080;
 
-const mongoUri =
-  "mongodb+srv://SevenNights:16081974Cfif@sevennights.ffufgve.mongodb.net/NewsApp?retryWrites=true&w=majority";
+const mongoUri = process.env.MONGODB_URI || "";
 
 const app = express();
 
@@ -24,6 +27,9 @@ app.set("view engine", "ejs");
 app.use(express.static(path.resolve("public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use("/", viewsRouter);
+app.use("/api/user", userRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}...`);
---
+++ public/scripts/auth.js
@@ -0,0 +1,37 @@
+const endpoint = "http://localhost:8080/api";
+
+let user = null;
+
+const auth = async () => {
+  try {
+    const response = await fetch(`${endpoint}/user/auth`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: "feff",
+        password: "fewfwfe",
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error("server error");
+    }
+
+    const data = await response.json();
+
+    user = data;
+
+    localStorage.setItem("user", JSON.stringify(user));
+
+    const event = new CustomEvent("userChanged", { detail: data });
+    window.dispatchEvent(event);
+  } catch (error) {
+    console.log("error:", error);
+  }
+};
+
+const logOut = () => {};
+
+export { user, auth, logOut };
